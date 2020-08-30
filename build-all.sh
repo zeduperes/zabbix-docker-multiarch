a86cc6589b30d1e09839857b759e51996e64d80e
Jose Peres <joseeduardoperes@icloud.com>
2020-08-30 18:40:31 -0300

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,7 @@ list_targets() {
   local tmpdir
   tmpdir="$(mktemp -d)"
   git clone -q --depth 1 https://github.com/zabbix/zabbix-docker "$tmpdir"
-  find "$tmpdir" -maxdepth 2 -mindepth 2 -type d | grep -v .git | \
+  find "$tmpdir" -maxdepth 5 -mindepth 5 -type d | grep -v .git | \
     sed -e "s|^${tmpdir}/||" -e 's|/|-|'
   trap "rm -rf \"$tmpdir\"" EXIT
 }
